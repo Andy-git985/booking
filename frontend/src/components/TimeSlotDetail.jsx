293a9db7a25cbbed477b9ba7912d4b6d9411b88c
Andy-git985 <andy.yu617@gmail.com>
2023-03-05 21:09:35 -0500

    Avatar for time slot details

--- frontend/src/components/TimeSlotDetail.jsx
+++ frontend/src/components/TimeSlotDetail.jsx
@@ -1,5 +1,10 @@
 import { Paper, Stack } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import Container from '@mui/material/Container';
 import { styled } from '@mui/material/styles';
+import andre from '../assets/images/andre-reis-_XD3D9pH83k-unsplash.jpg';
+import obi from '../assets/images/obi-pixel7propix--sRVfY0f2d8-unsplash.jpg';
+import salah from '../assets/images/salah-regouane-Z2WfmQC-sVk-unsplash.jpg';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -8,6 +13,10 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: 'center',
   color: theme.palette.text.secondary,
   cursor: 'pointer',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '2rem',
 }));
 
 const TimeSlotDetail = ({ available, selectPerson }) => {
@@ -15,19 +24,24 @@ const TimeSlotDetail = ({ available, selectPerson }) => {
     selectPerson({ id, email, disabled: false });
   };
   return (
-    <>
+    <Container sx={{ mt: 2, mb: 2 }}>
       <Stack spacing={2}>
         {available.map((person) => {
           return (
             <div key={person.id}>
-              <Item onClick={() => handleClick(person.id, person.email)}>
+              <Item onClick={(i) => handleClick(person.id, person.email)}>
+                <Avatar
+                  alt="Remy Sharp"
+                  src={andre}
+                  sx={{ width: 56, height: 56 }}
+                />
                 {person.email}
               </Item>
             </div>
           );
         })}
       </Stack>
-    </>
+    </Container>
   );
 };
 
